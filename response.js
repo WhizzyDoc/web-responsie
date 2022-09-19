f03a5abdc503cebe3b5d601a8fb9626a97ad33cb
WhizzyDoc <hassanridwan2536@gmail.com>
2022-09-19 01:09:56 +0100

    a

---
+++ response.js
@@ -0,0 +1,121 @@
+const userTexts = [
+    //1
+    ["hi", "hey", "hello", "good morning", "good afternoon", "good day"],
+    //2
+    ["how are you", "how is life", "how are things", "how are you doing", "are you doing good",
+     "are you fine", "how is your day going", "how is your day", "what's up", "whats up", "you good"],
+     //3
+    ["what are you doing", "what is going on", "what is up", "what's up", "whats up",
+     "you good", "xup"],
+     //4
+    ["how is your day", "how was your day", "how did your day go", "how's your day going",
+    "how's your day", "hows your day", "how's your day going", "how is your day going"],
+    //5
+    ["how old are you", "are you old"],
+    //6
+    ["who are you", "are you human", "are you bot", "are you human or bot"],
+    //7
+    ["who created you", "who made you", "were you created"],
+    //8
+    [
+      "your name please",
+      "your name",
+      "may i know your name",
+      "what is your name",
+      "what call yourself",
+      "What are you called",
+      "What do i call you",
+      "do you have a name",
+      "tell me your name"
+    ],
+    //9
+    ["i love you", "i like you"],
+    //10
+    ["happy", "good", "fun", "wonderful", "fantastic", "cool", "nice", "lovely"],
+    //11
+    ["bad", "bored", "tired", "sad"],
+    //12
+    ["help me", "tell me story", "tell me joke", "im bored"],
+    //13
+    ["ah", "yes", "ok", "okay", "nice", "hmm", "hmmm", "hm"],
+    //14
+    ["bye", "good bye", "goodbye", "see you later", "see ya"],
+    //15
+    ["what should i eat today", "what do i cook today"],
+    //16
+    ["bro"],
+    //17
+    ["what", "why", "how", "where", "when"],
+    //18
+    ["no","not sure","maybe","no thanks"],
+    //19
+    [""],
+    //20
+    ["haha","ha","lol","hehe","funny","joke","lmao"],
+    //21
+    ["im ok", "im good", "im okay", "im fine", "good"]
+  ]
+
+const botReplies = [
+    //1
+    ["Hello!", "Hi!", "Hey!", "Hi there!","Howdy"],
+    //2
+    [
+      "Fine... and you?",
+      "Pretty well, and you?",
+      "Fantastic, and you?"
+    ],
+    //3
+    [
+      "Nothing much",
+      "About to go to sleep",
+      "I'm just chilling",
+      "I don't know actually"
+    ],
+    //4
+    ["Been good so far"],
+    //5
+    ["I am infinite"],
+    //6
+    ["I am just a bot", "I am a bot. What are you?"],
+    //7
+    ["I was created by Rigan Tech"],
+    //8
+    ["I am nameless", "I don't have a name"],
+    //9
+    ["I love you too", "Me too"],
+    //10
+    ["Have you ever felt bad?", "Glad to hear it"],
+    //11
+    ["Why?", "Why? You shouldn't!", "Try watching TV"],
+    //12
+    ["What about?", "Once upon a time... that's the end of my story"],
+    //13
+    ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+    //14
+    ["Bye", "Goodbye", "See you later"],
+    //15
+    ["Sushi", "Pizza"],
+    //16
+    ["Bro!"],
+    //17
+    ["Great question"],
+    //18
+    ["That's ok","I understand","What do you want to talk about?"],
+    //19
+    ["Please say something :("],
+    //20
+    ["Haha!","Good one!"],
+    //21
+    ["that's nice", "that's good", "nice", "okay"]
+  ]
+    
+const alternative = [
+    "Same",
+    "Go on...",
+    "Bro...",
+    "Try again",
+    "I'm listening...",
+    "I don't understand :/"
+  ]
+  
